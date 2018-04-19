58607922423fd0e425679182cb8ba3f14739e3d4
michel <michael.kraemer@innoq.com>
2018-04-19 17:49:01 +0200

    merging

--- springfield_chain/mining/mining.py
+++ springfield_chain/mining/mining.py
@@ -15,6 +15,9 @@ def mine_block(block, proof=0, n=10000000):
         h = hash_api.hash_block_with_proof(block, p)
         if is_proven_hash(h):
             return p
+        else:
+            p += 1
+    return None
 
 #def mine_block_parallel(block):
  #   subprocess
