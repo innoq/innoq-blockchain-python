399468463655c4538bc39f753e015a9834b7ac52
Christine <christine@example.com>
2018-04-19 14:51:33 +0200

    return blocks

--- springfield_chain/web_api/api.py
+++ springfield_chain/web_api/api.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from springfield_chain.block.chain import BlockChain
 import uuid
+import json
 
 app = Flask(__name__)
 
@@ -13,16 +14,17 @@ def get_node_information():
         return '{"nodeId": "' + UUID + '", "currentBlockHeight": ' + str(len(chain)) +'}'
 
 
+#FIXME returns wrong data structure
 @app.route('/blocks')
 def get_blocks():
-        return "blocks"
+        return json.dumps(chain.__dict__)
 
-
+#FIXME implement
 @app.route('/mine')
 def get_mine():
         return "dummy miner"
 
-
+# FIXME implement
 @app.route("/nodes/register", methods=["POST"])
 def register_node ():
         return "dummy registration"
