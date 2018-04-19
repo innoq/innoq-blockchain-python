a695adf2c57153bc5b8355bc2778db0da286ada5
henning <henning.nobbe@innoq.com>
2018-04-19 13:22:09 +0200

    hash_api refactored

---
+++ springfield_chain/hashing/hash_api.py
@@ -0,0 +1,14 @@
+from hashlib import sha256
+import json
+
+ENCODING = "utf-8"
+
+def hashBlock(block):
+    block_as_json = json.dumps(block, separators=(",", ":"), sort_keys=True)
+    hash = sha256(block_as_json.encode(ENCODING)).hexdigest()
+    return hash
+
+
+def hashBlockWithProof(block, proof):
+    block["proof"] = proof
+    return hashBlock(block)
