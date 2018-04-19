b5c99ae5d970d3252f59715e9532f2ad38ee2b5a
Christine <christine@example.com>
2018-04-19 14:24:40 +0200

    generate uuid for node

--- springfield_chain/web_api/api.py
+++ springfield_chain/web_api/api.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from springfield_chain.block import blocks
+import uuid
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_node_information():
-        return '{"nodeId": "Hello, World!"}'
+        return '{"nodeId": "' + str(uuid.uuid1()) + '", "currentBlockHeight": 0}'
 
 
 @app.route('/blocks')
 def get_blocks():
         return blocks.dummyBlocks()
 
+
 @app.route('/mine')
 def get_mine():
         return '"Hello, mine!'
+
+
+@app.route("/nodes/register", methods=["POST"])
+def register_node ():
+        return "dummy registration"
