60a0afa1b5da44ff953f3058ca0097a261fedd3d
Oliver Tigges <oliver.tigges@me.com>
2018-04-19 16:37:47 +0200

    Improved hashing API.

--- springfield_chain/hashing/hash_api.py
+++ springfield_chain/hashing/hash_api.py
@@ -1,6 +1,6 @@
 from hashlib import sha256
 import json
-from springfield_chain.block.blocks import Block
+from springfield_chain.block.blocks import Block, block_from_dict
 
 ENCODING = "utf-8"
 
@@ -8,10 +8,7 @@ def hashBlock(data):
     return hashBlockWithProof(data, data["proof"])
 
 def hashBlockWithProof(data, proof):
-    block = Block(data["index"], data["previousBlockHash"], data["timestamp"])
-    for tx in data["transactions"]:
-        block.add_transaction(tx["id"], tx["timestamp"], tx["payload"])
+    block = block_from_dict(data)
     block.proof = proof
     block_as_json = json.dumps(block.to_ordered_dict(), separators=(",", ":"), sort_keys=False)
-    hash = sha256(block_as_json.encode(ENCODING)).hexdigest()
-    return hash
+    return sha256(block_as_json.encode(ENCODING)).hexdigest()
