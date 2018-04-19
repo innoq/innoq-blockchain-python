a4c0575591d9743e5b9a6fae08cddf6b717b712d
Christine <christine@example.com>
2018-04-19 16:56:45 +0200

    prepare including mining in web api

--- springfield_chain/web_api/api.py
+++ springfield_chain/web_api/api.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
-import json
 import time
 from springfield_chain.web_api.node import Node
 from springfield_chain.block.blocks import Block
 from springfield_chain.hashing import hash_api
-
+from springfield_chain.mining import mining
 
 app = Flask(__name__)
 
@@ -28,7 +27,8 @@ def get_mine():
         last_hash = hash_api.hashBlock(last_block)
         print (str(last_block))
         new_block = Block(last_block['index'] + 1, last_hash)
-        # FIXME generate and add proof
+        #proof = mining.mine_block(new_block)
+        #new_block.set_proof(proof)
         node.chain.append(new_block)
         time_end = time.clock()
         return '{"message": "Mined a new block in ' + str(time_end-time_start) + 's", "block":' + new_block.toJSON() + '}'
