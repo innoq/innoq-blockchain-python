1fb6a171040f6e56f6bf2955d524d487df076c6f
Oliver Tigges <oliver.tigges@me.com>
2018-04-19 16:49:48 +0200

    Refactored hashing.

--- springfield_chain/hashing/hash_api.py
+++ springfield_chain/hashing/hash_api.py
@@ -1,14 +1,13 @@
 from hashlib import sha256
 import json
-from springfield_chain.block.blocks import Block, block_from_dict
 
 ENCODING = "utf-8"
 
-def hashBlock(data):
-    return hashBlockWithProof(data, data["proof"])
-
-def hashBlockWithProof(data, proof):
-    block = block_from_dict(data)
-    block.proof = proof
+def hash_block(block):
     block_as_json = json.dumps(block.to_ordered_dict(), separators=(",", ":"), sort_keys=False)
     return sha256(block_as_json.encode(ENCODING)).hexdigest()
+
+
+def hash_block_with_proof(block, proof):
+    block.proof = proof
+    return hash_block(block)
--- springfield_chain/hashing/hash_test.py
+++ springfield_chain/hashing/hash_test.py
@@ -1,8 +1,9 @@
 import json
 
-from springfield_chain.block.blocks import Block
+from springfield_chain.block.blocks import Block, block_from_dict
 from springfield_chain.hashing import hash_api
 
 with open('../block/genesis.json') as json_data:
     data = json.load(json_data)
-    print(hash_api.hashBlockWithProof(data, 1917336))
+    block = block_from_dict(data)
+    print(hash_api.hash_block_with_proof(block, 1917336))
