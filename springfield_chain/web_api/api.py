9ccaf599993fd44944bf39852811401335621b94
Christine <christine@example.com>
2018-04-19 14:33:57 +0200

    refactoring

--- springfield_chain/web_api/api.py
+++ springfield_chain/web_api/api.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from springfield_chain.block import blocks
 import uuid
+from springfield_chain.block.chain import BlockChain
+import json
 
 app = Flask(__name__)
+
+with open('springfield_chain/block/genesis.json') as in_file:
+    genesis = json.loads(in_file.read())
+
+chain = BlockChain(genesis)
 
 UUID = str(uuid.uuid1())
 
 @app.route('/')
 def get_node_information():
-        return '{"nodeId": "' + UUID + '", "currentBlockHeight": 0}'
+        return '{"nodeId": "' + UUID + '", "currentBlockHeight": ' + str(len(chain)) +'}'
 
 
 @app.route('/blocks')
