47579aeeba5285b0da8ce039f3cc2d8ef584786c
Christine <christine@example.com>
2018-04-19 17:40:43 +0200

    readd miningt tests

---
+++ springfield_chain/mining/mining_test.py
@@ -0,0 +1,8 @@
+import json
+
+from springfield_chain.mining import mining
+
+with open('../block/genesis.json') as json_data:
+    data = json.load(json_data)
+    proof = mining.mine_block(data, 1900000, 100000)
+    print("Mined proof: " + proof)
