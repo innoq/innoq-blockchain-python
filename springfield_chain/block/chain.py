65578d5a25b56cb55116955573fe0a177a82b593
dsInnoq <38533253+dsInnoq@users.noreply.github.com>
2018-04-19 17:58:05 +0200

    check and append new block - draft

--- springfield_chain/block/chain.py
+++ springfield_chain/block/chain.py
@@ -1,5 +1,9 @@
 import json
 
+from springfield_chain.hashing.hash_api import hash_block
+from springfield_chain.mining.mining import check_block
+
+
 class BlockChain(list):
     """
     represents the whole chain
@@ -8,10 +12,30 @@ class BlockChain(list):
 
     def __init__(self, genesis):
         super().__init__([genesis])
+        self.last_hash = hash_block(genesis)
 
     def append(self, new_block):
-        # TODO check integrity here?
-        super().append(new_block)
+        """
+        Checks a candidate block and (upon successful checks) adds it to the chain
+        :param new_block:
+        :return: true if successful, false if block was rejected
+        """
+        if self.check_new_block_validity(new_block):
+            super().append(new_block)
+            self.last_hash = hash_block(new_block) # TODO avoid computing hash twice
+            return True
+        else:
+            return False
+
+    def check_new_block_validity(self, new_block):
+        if self.last_hash != new_block["previousBlockHash"]:
+            print("last hash mismatch")
+            return False
+        elif check_block(new_block):
+            print("invalid new block")
+            return False
+        else:
+            return True
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
