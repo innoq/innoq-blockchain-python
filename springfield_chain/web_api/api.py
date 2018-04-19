830bd1af2557b2acaa569ba2bda25bf5ede98729
Christine <christine@example.com>
2018-04-19 16:34:48 +0200

    add time measurement

--- springfield_chain/web_api/api.py
+++ springfield_chain/web_api/api.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 import json
+import time
 from springfield_chain.web_api.node import Node
 from springfield_chain.block.blocks import Block
 from springfield_chain.hashing import hash_api
@@ -22,13 +23,15 @@ def get_blocks():
 
 @app.route('/mine')
 def get_mine():
+        time_start = time.clock()
         last_block = node.chain[-1]
         last_hash = hash_api.hashBlock(last_block)
         print (str(last_block))
         new_block = Block(last_block['index'] + 1, last_hash)
         # FIXME generate and add proof
         node.chain.append(new_block)
-        return '{"message": "Mined a new block in xx.xxx s", "block":' + new_block.toJSON() + '}'
+        time_end = time.clock()
+        return '{"message": "Mined a new block in ' + str(time_end-time_start) + 's", "block":' + new_block.toJSON() + '}'
 
 # FIXME implement
 @app.route("/nodes/register", methods=["POST"])
